2acf17a88a5ca5b265d7bdab7c7e40acdad037ef
Marcel Dokters <mdokters@uni-osnabrueck.de>
2022-02-15 18:52:16 +0100

    Added new encoding

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ast import Index
 from random import seed
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
@@ -27,10 +28,11 @@ def write_metrics_binary_classification(clf, file_to_write, X, y):
     for train_index, test_index in kf.split(X,y):
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-        clf.fit(X_train, y_train)
-        print(clf.score(X_test,y_test))
-    print(X.head())
-    print(y.head())
+        print(X_train)
+        #clf.fit(X_train, y_train)
+        #print(clf.score(X_test,y_test))
+    #print(X.head())
+    #print(y.head())
     clf.fit(X,y)
     print(clf.score(X,y))
 
@@ -39,32 +41,70 @@ if __name__ == "__main__":
     titanic_df = pd.read_csv("./datasets/titanic.csv")
     heart_df = pd.read_csv("./datasets/heart.csv")
     wine_red_df = pd.read_csv("./datasets/winequality-red.csv")
-    wine_white_df = pd.read_csv("./datasets/winequality-white.csv")
+    wine_white_df = pd.read_csv("./datasets/winequality-white.csv",sep=";")
     # clean dataframes of useless data ( titanic only) and get class as last element in df
     titanic_df = titanic_df.drop("Name",axis=1)
     titanic_df["Sex"] = titanic_df["Sex"].map({"female" : 1, "male" : 0}).astype(int)
-    print(titanic_df.head())
+    #print(titanic_df.head())
     ## Benchmarking
     rf = RandomForestClassifier(random_state=seed)
     #write_metrics_binary_classification(rf,"./results/rf_titanic.txt",titanic_df.drop(["Survived"],axis=1),titanic_df["Survived"]) 
     # binning
     titanic_df["Fare"] = pd.qcut(titanic_df["Fare"],5,labels=False)
     titanic_df["Age"] = pd.qcut(titanic_df["Age"],10,labels=False)
-    print(titanic_df.head())
+    #print(titanic_df.head())
     le = LabelEncoder()
     titanic_df_le = titanic_df.apply(le.fit_transform)
     titanic_df_hot_one = titanic_df_le
-    features = ["Fare","Age", "Siblings/Spouses Aboard","Parents/Children Aboard"]
-    for feature in features:
+    titanic_features = ["Fare","Age", "Siblings/Spouses Aboard","Parents/Children Aboard","Pclass"]
+    for feature in titanic_features:
         titanic_df_hot_one = encode_and_bind(titanic_df_hot_one,feature)
     col_list = list(titanic_df)
     col_list[0], col_list[-1] = col_list[-1], col_list[0]
     titanic_df = titanic_df[col_list]
-    print(titanic_df.head())
+    #print(titanic_df.head())
     #
     col_list = list(titanic_df_hot_one)
     col_list[0], col_list[-1] = col_list[-1], col_list[0]
     titanic_df_hot_one = titanic_df_hot_one[col_list]
-    print(titanic_df_hot_one.head())
+    #print(titanic_df_hot_one.head())
+    # preprocess heart_csv
+    heart_df_binned = heart_df
+    heart_df_binned['age'] = pd.to_numeric(pd.cut(heart_df['age'],9,labels=[0,1,2,3,4,5,6,7,8]),downcast="float")
+    heart_df_binned['trestbps'] = pd.to_numeric(pd.cut(heart_df['trestbps'],10,labels=[0,1,2,3,4,5,6,7,8,9]),downcast="float")
+    heart_df_binned['chol'] = pd.to_numeric(pd.cut(heart_df['chol'],7,labels=[0,1,2,3,4,5,6]),downcast="float")
+    heart_df_binned['thalach'] = pd.to_numeric((pd.cut(heart_df['thalach'],10,labels=[0,1,2,3,4,5,6,7,8,9])),downcast="float")
+    heart_df_binned['oldpeak'] = pd.to_numeric((pd.cut(heart_df['oldpeak'],7,labels=[0,1,2,3,4,5,6])),downcast="float")
+    le = LabelEncoder()
+    heart_df_binned = heart_df_binned.apply(le.fit_transform)
+    #print(heart_df_binned.head())
+    heart_df_hot_one = heart_df_binned
+    heart_features = ["age","sex","cp","trestbps","chol","thalach","oldpeak","slope","thal","ca","restecg"]
+    for feature in heart_features:
+        heart_df_hot_one = encode_and_bind(heart_df_hot_one,feature)
+    #print(heart_df_hot_one.to_numpy())
     #encode_and_bind()
-    
+    #feature_list = list(heart_df_hot_one.drop(["target"]))
+    X = heart_df_hot_one.drop(["target"],axis=1)
+    #write_metrics_binary_classification(corels_clf, None, X,y)
+    #write_metrics_binary_classification(corels_clf,None,titanic_df_hot_one.drop(["Survived"],axis=1),titanic_df_hot_one["Survived"])
+    # preprocess wine white dataset
+    #print(wine_white_df.head())
+    wwine_quality = wine_white_df["quality"]
+    wine_white_df_binned = wine_white_df.drop(["quality"],axis=1)
+    for feature in list(wine_white_df_binned):
+        wine_white_df_binned[feature] = pd.qcut(wine_white_df_binned[feature],5,labels=False)
+    le = LabelEncoder()
+    wine_white_df_binned = wine_white_df_binned.apply(le.fit_transform)
+    #print(wine_white_df_binned.head())
+    wine_white_df_hot_one = wine_white_df_binned
+    for feature in list(wine_white_df_hot_one):
+        wine_white_df_hot_one = encode_and_bind(wine_white_df_hot_one,feature)
+    clf = corels.CorelsClassifier(max_card=5)
+    wwine_quality = pd.qcut(wwine_quality,2,labels=[1,2])
+    wwine_quality_hot_one = pd.get_dummies(wwine_quality)
+    clf.fit(wine_white_df_hot_one,wwine_quality_hot_one[1],features=list(wine_white_df_hot_one))
+    print(clf.score(wine_white_df_hot_one,wwine_quality_hot_one[1]))
+    rf = RandomForestClassifier()
+    rf.fit(wine_white_df_hot_one,wwine_quality_hot_one[1])
+    print(rf.score(wine_white_df_hot_one,wwine_quality_hot_one[1]))
